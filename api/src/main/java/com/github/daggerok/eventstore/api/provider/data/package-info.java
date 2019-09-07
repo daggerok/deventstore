c4cfece7101b582afb542875f6ee5e4125bc7ef7
Maksim Kostromin <daggerok@gmail.com>
2019-09-07 23:58:02 +0300

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL (C) whatthecommit.com

--- api/src/main/java/com/github/daggerok/eventstore/api/provider/data/package-info.java
+++ api/src/main/java/com/github/daggerok/eventstore/api/provider/data/package-info.java
@@ -1,5 +1,19 @@
 /**
  * EventStore can be in-memory or persistent, but in any case
  * it should use some abstraction around data its manipulating...
+ *
+ * DataProvider examples:
+ *
+ * InMemoryDataProvider:
+ * - ConcurrentHashMapEventLog
+ * FilesystemDataProvider:
+ * - FileAppendOnlyEventLog
+ * RdbmsDataProvider:
+ * - H2EventLog
+ * - MysqlEventLog
+ * - PostgresqlEventLog
+ * NoSqlDataProvider:
+ * - MongodbEventLog
+ * - RedisEventLog
  */
 package com.github.daggerok.eventstore.api.provider.data;
