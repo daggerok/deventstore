856b08f89773965a7e2dc64053259fee45bf5f06
Maksim Kostromin <daggerok@gmail.com>
2019-09-07 21:59:47 +0300

    work in progress (C) whatthecommit.com

--- api/src/main/java/com/github/daggerok/eventstore/api/provider/data/package-info.java
+++ api/src/main/java/com/github/daggerok/eventstore/api/provider/data/package-info.java
@@ -1,4 +1,5 @@
 /**
- * EventStore should relay on some serialization / deserialization mechanism
+ * EventStore can be in-memory or persistent, but in any case
+ * it should use some abstraction around data its manipulating...
  */
 package com.github.daggerok.eventstore.api.provider.data;
