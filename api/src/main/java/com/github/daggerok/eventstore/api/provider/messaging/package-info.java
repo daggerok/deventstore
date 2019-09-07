f64bf2678dbfdde9e1fbc3586d7c18f9bf963e33
Maksim Kostromin <daggerok@gmail.com>
2019-09-07 23:58:02 +0300

    SEXY RUSSIAN CODES WAITING FOR YOU TO CALL (C) whatthecommit.com

--- api/src/main/java/com/github/daggerok/eventstore/api/provider/messaging/package-info.java
+++ api/src/main/java/com/github/daggerok/eventstore/api/provider/messaging/package-info.java
@@ -1,4 +1,15 @@
 /**
  * EventStore should relay on messaging to receive, react and replay on different activities happening around it
+ *
+ * MessagingProvider examples:
+ *
+ * InMemoryMessagingProvider:
+ * - BlockingQueueTransport
+ * - EmbeddedActivemqTransport
+ * BrokerMessagingProvider:
+ * - ActivemqTransport
+ * - RedisTransport
+ * - RabbitmqTransport
+ * - KafkaTransport
  */
 package com.github.daggerok.eventstore.api.provider.messaging;
--- api/src/main/java/com/github/daggerok/eventstore/api/provider/serialisation/package-info.java
+++ api/src/main/java/com/github/daggerok/eventstore/api/provider/serialisation/package-info.java
@@ -1,4 +1,18 @@
 /**
  * EventStore should relay on some serialization / deserialization mechanism
+ *
+ * SerializationProvider examples:
+ *
+ * JsonSerializationProvider
+ * - JacksonJsonValueSerializer
+ * - JacksonJsonValueDeserializer
+ * - JsonbSerializer
+ * - JsonbDeserializer
+ * XmlSerializationProvider
+ * - JacksonXmlSerializer
+ * - JacksonXmlDeserializer
+ * BinarySerialisationProvider
+ * - ProtoBufSerializer
+ * - ProtoBufDeserializer
  */
 package com.github.daggerok.eventstore.api.provider.serialisation;
